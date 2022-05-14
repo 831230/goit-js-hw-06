1920aeebf150cb4079b81e3c6cb1cf49af1f7739
Tomasz Bielecki <szkoleniegoit@gmail.com>
2022-05-14 20:05:10 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,9 +1,11 @@
 const decrement = document.querySelector('button[data-action="decrement"]');
 const increment = document.querySelector('button[data-action="increment"]');
 
+const one = 1;
+
 const counterValue = document.querySelector('#value');
 
 decrement.addEventListener("click", function () { 
-    counterValue.innerHTML = counterValue-1;
+    counterValue.innerHTML = counterValue-Number(one);
     return counterValue;
 })
