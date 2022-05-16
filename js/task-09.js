2e3f5d8f1601b3c9a36ad188f0077cfad650c05e
Tomasz Bielecki <szkoleniegoit@gmail.com>
2022-05-16 12:15:25 +0200

    Zadanie nr 9
    Ukończone

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,9 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+const colorBtn = document.querySelector(".change-color");
+const resultValueColor = document.querySelector(".color");
+const bodyBg = document.querySelector("body");
+
+
+colorBtn.addEventListener("click", function getRandomHexColor() {
+  bodyBg.style.backgroundColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  resultValueColor.innerHTML = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+});
