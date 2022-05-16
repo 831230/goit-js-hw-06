a0e4a4061562b7dd2e2d5d2d4598678ee3b6cfb6
Tomasz Bielecki <szkoleniegoit@gmail.com>
2022-05-16 11:49:36 +0200

    Zadanie nr 8
    Ukończone

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,22 @@
+const form = document.querySelector(".login-form");
 
+const submitForm = (evt) => {
+  const form = evt.currentTarget;
+  evt.preventDefault();
+    console.log(form.elements.email.value);
+    console.log(form.elements.password.value);
+
+    const {
+        elements: { email, password },
+    } = form;
+    
+    if (!email.value||!password.value) { 
+        return alert("Wszystkie pola muszą być uzupełnione");
+    };
+
+    console.log(`email: ${email.value}, Password: ${password.value}`);
+
+    evt.currentTarget.reset();
+};
+
+form.addEventListener("submit", submitForm);
