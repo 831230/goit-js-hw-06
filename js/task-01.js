1b3331074979293c3e793ac6dd9e2bb3843f0693
Tomasz Bielecki <szkoleniegoit@gmail.com>
2022-05-16 14:05:05 +0200

    Zadanie nr 1
    Ukończone

--- js/task-01.js
+++ js/task-01.js
@@ -1,1 +1,7 @@
+const arrayCategories = document.querySelectorAll(".item");
+console.log(`Number of categories: ${arrayCategories.length}`);
 
+arrayCategories.forEach(function (category) {
+    console.log(`Category: ${category.firstElementChild.textContent}`);
+    console.log(`Elements: ${category.querySelectorAll("li").length}`);
+});
